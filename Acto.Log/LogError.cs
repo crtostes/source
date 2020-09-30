ea97e7e15088db7f2a7b44e10cf0b52f6e061bad
crtostes <crtostes@globo.com>
2020-09-29 22:07:20 -0300

    versão 29/09/2020

---
+++ Acto.Log/LogError.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acto.Infra.Log
+{
+    public class LogError
+    {
+        public void TrataErro(string pStackTrace)
+        {
+            System.IO.StreamWriter sr = new System.IO.StreamWriter("C:\\Temp\\ErrorLogBT.txt");
+
+            sr.WriteLine(pStackTrace);
+            sr.Close();
+            
+        }
+    }
+}
